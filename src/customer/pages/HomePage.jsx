e72ddb170609734708360cf6fee50eb91c6bc07e
Nikhil Negi <157276223+neginikhil@users.noreply.github.com>
2024-06-10 11:15:40 +0530

    Initializing

---
+++ src/customer/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import MainCarousel from "../components/HomeCarousel/MainCarousel";
+
+const HomePage = () => {
+    return <div>
+        <MainCarousel />
+    </div>
+}
+export default HomePage;
